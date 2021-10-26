4d5302ed9c66ea75f0eb5b8ba90e78a99b790c8f
mivlev1 <makzoyn@gmail.com>
2021-10-26 11:34:55 +0300

    edit js and html

--- js/main.js
+++ js/main.js
@@ -19,41 +19,119 @@ window.addEventListener('DOMContentLoaded', function (total) {
   b.addEventListener("click", total);
 });
 //калькулятор 2
-const basePrice=1000;
-const inputs=document.querySelectorAll('input');
-const totalPriceElement=document.querySelector('#total-price');
-//количество
-const countOf=document.querySelector('input[name="count"]')
-//список
-const typeList=document.querySelector('select');
-//радиокнопки
-const radioOptions = document.querySelectorAll('input[name="option"]');
-//чекбоксы
-const checkFirst=document.querySelector('input[name="first-check"]');
-const checkSecond = document.querySelector('input[name="second-check"]');
- 
-function calc() {
-  let totalPrice=basePrice;
-  totalPrice=totalPrice*parseInt(countOf.value);
-  
-  for(const radio of radioOptions){
-    if(radio.checked){
-      totalPrice = totalPrice*parseFloat(radio.value);
+function updatePrice() {
+  let price = 0;
+  let prices = getPrices();
+  let priceIndex = parseInt(select.value) - 1;
+  if (priceIndex >= 0) {
+    price = prices.prodTypes[priceIndex];
+  }
+  let quantity = Number(document.getElementById('select_input').value);
+  let check_div_Div = document.querySelectorAll('#check_div input');
+  check_div_Div.forEach(function (checkbox) {
+    if (checkbox.checked) {
+      let propPrice = prices.prodProperties[checkbox.name];
+      if (propPrice !== undefined) { price += propPrice; }
     }
-  }
-  if(checkFirst.checked){
-    totalPrice=totalPrice*parseFloat(checkFirst.value);
-  }
-  if (checkSecond.checked) {
-    totalPrice = totalPrice * parseFloat(checkSecond.value);
-  }
-  
-  const formatter = new Intl.NumberFormat('ru');
-  totalPriceElement.innerText=totalPrice;
+  });
+  let radio_options = document.getElementsByName('radio_options');
+  let k = 0;
+  radio_options.forEach(function (radio) {
+    if (radio.checked) {
+      let optionPrice = prices.radio_options[radio.value];
+      if (optionPrice !== undefined) {
+        price += optionPrice;
 
+      }
+    }
+  });
+
+
+  let cou = document.getElementById('select_input');
+  cou.addEventListener('input', function () { updatePrice(); });
+  let s = document.getElementsByName('type');
+  s[0].addEventListener('change', function (event) {
+    let select = event.target;
+    let radio_div = document.getElementById('radio_div');
+    let check_div = document.getElementById('check_div');
+    if (select.value === '1') {
+      radio_div.style.display = 'none';
+      check_div.style.display = 'none';
+      radio_id();
+      check_div_hide();
+      document.getElementById('price2').innerHTML =
+        'Цена: ' + prices.prodTypes[0] * quantity + ' рублей';
+    }
+    else if (select.value === '2') {
+      check_div_hide();
+      radio_div.style.display = 'block';
+      check_div.style.display = 'none';
+      document.getElementById('price2').innerHTML =
+        'Цена: ' + prices.prodTypes[1] * quantity + ' рублей.';
+    }
+    else if (select.value === '3') {
+      radio_div.style.display = 'none';
+      check_div.style.display = 'block';
+      radio_id();
+      document.getElementById('price2').innerHTML =
+        'Цена: ' + prices.prodTypes[2] * quantity + ' рублей.';
+    } else {
+    }
+  });
+  let price_pr = document.getElementById('price2');
+  price_pr.innerHTML = 'Цена: ' + price * quantity + ' рублей.';
 }
-for(const input of inputs){
-  input.addEventListener('input',function(){
-    calc();
-  })
+function getPrices() {
+  return {
+    prodTypes: [100, 1000, 5000],
+    radio_options: {
+      UnderTheHood: 100,
+      Salon: 500,
+      Outdoor: 1000,
+    },
+    prodProperties: {
+      Polishing: 50,
+      Orange: 500,
+      Chips: 1500,
+    },
+  };
 }
+let elm = document.getElementById('radio_id');
+elm.style.display = 'none';
+function radio_id() {
+  elm.checked = !elm.checked;
+}
+function check_div_hide() {
+  document.getElementById('Polishing').checked = false;
+  document.getElementById('Painting').checked = false;
+  document.getElementById('Chips').checked = false;
+}
+
+
+
+window.addEventListener('DOMContentLoaded', function () {
+
+
+
+  let radioDiv = document.getElementById('radio_div');
+  radioDiv.style.display = 'none';
+
+  let check_div = document.getElementById('check_div');
+  check_div.style.display = 'none';
+
+  let s = document.getElementsByName('type');
+  let select = s[0];
+
+  select.addEventListener('change', function () {
+    updatePrice();
+  });
+  let radio_options = document.getElementsByName('radio_options');
+  radio_options.forEach(function (radio) {
+    radio.addEventListener('change', function () { updatePrice(); });
+  });
+  let sniper_option = document.querySelectorAll('#check_div input');
+  sniper_option.forEach(function (checkbox) {
+    checkbox.addEventListener('change', function () { updatePrice(); });
+  });
+  updatePrice();
+});
